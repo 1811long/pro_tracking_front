909061696d7cf6173c4204c70d739b7a145aaae3
Rinkichi75 <cocmithulan@gmail.com>
2022-06-11 23:48:52 +0200

    FEAT: ChampionPage

--- src/pages/ChampionPage.js
+++ src/pages/ChampionPage.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import NavBar from './../components/NavBar'
-import Container from '@material-ui/core/Container';
-import ChampCard from './../components/Card'
-import logo from './RiotX_ChampionList_irelia.jpg'
+import React, { useState, useEffect }from 'react';
+import NavBar from './../components/NavBar';
+import ChampCard from './../components/Card';
+import api from './../utils/api'
+import './ChampionPage.css'
+
 export default function ChampionPage() {
-  
+  const [champions, setChampions] = useState([]);
+
+  useEffect(() => {
+    const fetchChampions = async () => {
+      try{
+        const response = await api.get('/champions');
+        setChampions(response.data['result']);
+      }catch (err){
+        if (err.response){
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        }else{
+          console.log(`Error: ${err.message}`);
+        }
+      }   
+    }
+
+    fetchChampions();
+  }, [])
+
+
   return (
       <>
         <NavBar />
-        <ChampCard image={logo} name='Irelia'/>
+        <div className="card-container">
+          <div className="card-grid">
+            {!champions
+             ? 'pas assos'
+              : champions.map(({name, full, sprite }) => (
+                <>
+                  <div className="card-key" key={name}>
+                    <ChampCard name={name} image={full}/>
+                  </div>
+                </>
+              ))}
+          </div>
+        </div>
       </>
 )}
   
